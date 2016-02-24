56d7dd3efc6c1c3e1ea842182ff037ae48f49cdd
Keita Okamoto <ok@aaa.email.ne.jp>
2016-02-24 23:24:06 +0900

    ignore /src/vendor/*

--- src/vendor/@all-user/ok-blocks/test/index.test.js
+++
@@ -1,8 +0,0 @@
-// patterns
-import '@all-user/ok-patterns-lines'
-import '@all-user/ok-patterns-olympic2020'
-
-// test
-import './OKBlock.test.js'
-import './OKBlocksGroup.test.js'
-import './Lines.test.js'
